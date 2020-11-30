7ba8526ba0bd8c9b7feabd199494c59cdd6c5102
Jayna Leitze <jayna.leitze@gmail.com>
2020-11-30 15:14:57 -0600

    add animal

--- src/Components/animal/Animal.js
+++ src/Components/animal/Animal.js
@@ -7,6 +7,6 @@ export const Animal = ({ animal, customer, location }) => (
     <h3 className="animal__name">{animal.name}</h3>
     <div className="animal__breed">{animal.breed}</div>
     <div className="animal__location">{location.name}</div>
-    <div className="animal__owner">{customer.name}</div>
+    {/* <div className="animal__owner">{customer.name}</div> */}
   </section>
 );
